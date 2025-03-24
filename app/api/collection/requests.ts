5c4464626408c6463fc02d0951f167d195406934
Petar <petartopic2002@gmail.com>
2025-03-24 23:25:35 +0100

    Add fetch request with middleware for unathanticated requests

--- app/api/collection/requests.ts
+++ app/api/collection/requests.ts
@@ -1,67 +1,39 @@
 import { config } from '@/shared/config';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from '../constants';
+import fetchWithMiddleware from '../fetchWithMiddleware';
 
 const getCollections = async (
 	page = DEFAULT_PAGE,
 	searchQuery = '',
 	pageSize = DEFAULT_PAGE_SIZE
 ) => {
-	const accessToken = await AsyncStorage.getItem('accessToken');
-
 	const url = new URL(`${config.apiUrl}/collections`);
 	url.searchParams.set('page', page);
 	url.searchParams.set('searchQuery', searchQuery);
 	url.searchParams.set('pageSize', pageSize);
 
-	const response = await fetch(url, {
+	return await fetchWithMiddleware(url.toString(), {
 		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${accessToken}`,
-		},
 	});
-	return response.json();
 };
 
 const getCollection = async (id: string) => {
-	const accessToken = await AsyncStorage.getItem('accessToken');
-
-	const response = await fetch(`${config.apiUrl}/collections/${id}`, {
+	return await fetchWithMiddleware(`${config.apiUrl}/collections/${id}`, {
 		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${accessToken}`,
-		},
 	});
-	return response.json();
 };
 
 const createCollection = async (name: string, description: string) => {
-	const accessToken = await AsyncStorage.getItem('accessToken');
-
-	const response = await fetch(`${config.apiUrl}/collections`, {
+	return await fetchWithMiddleware(`${config.apiUrl}/collections`, {
 		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${accessToken}`,
-		},
 		body: JSON.stringify({ name, description }),
 	});
-	return response.json();
 };
 
 const deleteCollection = async (id: string) => {
-	const accessToken = await AsyncStorage.getItem('accessToken');
-
-	const response = await fetch(`${config.apiUrl}/collections/${id}`, {
+	return await fetchWithMiddleware(`${config.apiUrl}/collections/${id}`, {
 		method: 'DELETE',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${accessToken}`,
-		},
 	});
-	return response.json();
 };
 
 export default {
---
+++ app/api/fetchWithMiddleware.ts
@@ -0,0 +1,32 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
+
+const fetchWithMiddleware = async (url: string, options: RequestInit = {}) => {
+	const accessToken = await AsyncStorage.getItem('accessToken');
+
+	try {
+		const response = await fetch(`${url}`, {
+			method: options.method || 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				...(options.headers || {}),
+				Authorization: `Bearer ${accessToken}`,
+			},
+			body: options.body,
+		});
+
+		if (response.status === 401) {
+			router.navigate('/getStarted' as any);
+			return null;
+		}
+
+		const data = await response.json();
+
+		return data;
+	} catch (error) {
+		console.error('Network error:', error);
+		return null;
+	}
+};
+
+export default fetchWithMiddleware;
