c9bf55695aa6abc7d95de1a365f93442167790d8
Petar <petartopic2002@gmail.com>
2025-03-26 13:05:34 +0100

    add typography

--- app/components/Subtitle.tsx
+++ app/components/Subtitle.tsx
@@ -1,7 +1,7 @@
 import { Text } from 'react-native';
 
 const Subtitle = ({ children }: { children: React.ReactNode }) => {
-	return <Text className="text-lg font-bold">{children}</Text>;
+	return <Text className="heading-xxsmall">{children}</Text>;
 };
 
 export default Subtitle;
--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,7 +1,7 @@
 import { Text } from 'react-native';
 
 const Title = ({ children }: { children: React.ReactNode }) => {
-	return <Text className="text-2xl font-bold">{children}</Text>;
+	return <Text className="heading-small">{children}</Text>;
 };
 
 export default Title;
